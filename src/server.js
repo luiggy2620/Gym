29d36b5eb8acad15e7470f6fef3e17f061f8bd67
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-01-02 18:54:57 -0400

    build: :wrench: config in server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const indexRoutes = require('./routes/index.routes');
 const clientRoutes = require('./routes/client.routes');
@@ -9,9 +10,13 @@ const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('view', path.join(__dirname, 'view'));
 
 // midlewares
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 
 // global variables
 
