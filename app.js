ab63969a18fd3fe6b0ecc93332512857ba2ebade
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-01-09 18:34:11 -0400

    ci: :zap: configuration to deploy it

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const server = require('./server');
-require('./database');
+const server = require('./src/server');
+require('./src/database');
 
 server.listen(server.get('port'), () => {
     console.log(`server on port ${server.get('port')}`);
