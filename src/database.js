611eb8a750890231ceb484e655373f9f3ddf3d4e
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-01-09 20:56:07 -0400

    build: :wrench: modify database

--- src/database.js
+++ src/database.js
@@ -2,18 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
 
-module.exports = () => {
-    
-    mongoose.connect(
-        process.env.MONGO_URI, {
-        useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(
+    process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+    .then(db => {
+        console.log('Database is connected');
     })
-    
-        .then(db => {
-            console.log('Database is connected');
-        })
-        .catch(err => {
-            console.log(err);
-        })
-        
-}
+    .catch(err => {
+        console.log(err);
+    })
